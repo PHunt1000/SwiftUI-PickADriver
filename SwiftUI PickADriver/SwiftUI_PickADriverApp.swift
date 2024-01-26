fb4a687b8da496ff5b79e413b2bb76b914ae9be7
Peggy Hunt <phunt1000@gmail.com>
2024-01-26 09:41:18 -0600

    Initial Commit

---
+++ SwiftUI PickADriver/SwiftUI_PickADriverApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_PickADriverApp.swift
+//  SwiftUI PickADriver
+//
+//  Created by Peggy Hunt on 1/26/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_PickADriverApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
